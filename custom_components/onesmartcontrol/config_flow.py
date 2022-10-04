eb4e1e8a40743ac551fa52aa9d68c328c00f23d3
Pim <17709721+PimDoos@users.noreply.github.com>
2022-10-04 21:17:14 +0200

    Add reauthentication to config flow

--- custom_components/onesmartcontrol/config_flow.py
+++ custom_components/onesmartcontrol/config_flow.py
@@ -62,6 +62,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for One Smart Control."""
 
     VERSION = 1
+    reauth_entry: config_entries.ConfigEntry | None = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -90,6 +91,29 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
+    async def async_step_reauth(self, user_input=None):
+        """Perform reauth upon an API authentication error."""
+        self.reauth_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(self, user_input=None):
+        """Dialog that informs the user that reauth is required."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema({}),
+            )
+        return await self.async_step_user()
+
+    async def async_create_entry(self, data: dict) -> dict:
+        """Create an entry or update existing entry for reauth."""
+        if self.reauth_entry:
+            self.hass.config_entries.async_update_entry(self.reauth_entry, data=data)
+            await self.hass.config_entries.async_reload(self.reauth_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+        return await super().async_create_entry(data)
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
