PimDoos/onesmartcontrolha

commit 9e5f95a210bcbf0fd32757eb268e1f3b729b3961

Add reauthentication to config flow (#35)

custom_components/onesmartcontrol/config_flow.py
@@ -62,6 +62,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for One Smart Control."""
 
     VERSION = 1
+    reauth_entry: config_entries.ConfigEntry | None = None
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -90,6 +91,29 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
+    async def async_step_reauth(self, user_input=None):
+        """Perform reauth upon an API authentication error."""
+        self.reauth_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(self, user_input=None):
+        """Dialog that informs the user that reauth is required."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema({}),
+            )
+        return await self.async_step_user()
+
+    async def async_create_entry(self, data: dict) -> dict:
+        """Create an entry or update existing entry for reauth."""
+        if self.reauth_entry:
+            self.hass.config_entries.async_update_entry(self.reauth_entry, data=data)
+            await self.hass.config_entries.async_reload(self.reauth_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+        return await super().async_create_entry(data)
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""

commit b550d5a89f32e0049597c1340ea2fb10907d2918

Fix config flow regression from #35 (#39)
* Use `get` function instead of dict indexes

* Get `device_info` attributes with `get` function

* Fix config flow not working (regression from #35)

custom_components/onesmartcontrol/config_flow.py
@@ -85,7 +85,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title=info["title"], data=user_input)
+            return await self.async_create_entry(title=info.get("title"), data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
@@ -107,13 +107,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
         return await self.async_step_user()
 
-    async def async_create_entry(self, data: dict) -> dict:
+    async def async_create_entry(self, title: str, data: dict) -> dict:
         """Create an entry or update existing entry for reauth."""
         if self.reauth_entry:
             self.hass.config_entries.async_update_entry(self.reauth_entry, data=data)
             await self.hass.config_entries.async_reload(self.reauth_entry.entry_id)
             return self.async_abort(reason="reauth_successful")
-        return await super().async_create_entry(data)
+        return super().async_create_entry(title=title, data=data)
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""